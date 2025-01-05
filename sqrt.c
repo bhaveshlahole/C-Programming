b5af66f74eb53906ef54548fbbe83a2f9fb29364
BHAVESH NANDKUMAR LAHOLE <149697798+bhaveshlahole@users.noreply.github.com>
2025-01-05 13:56:45 +0530

    Add files via upload

---
+++ sqrt.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    printf("Enetr value for a");
+    scanf("%d",&a);
+    int root = sqrt(a);
+    printf(" Square root is %d ",root);
+    return 0;
+}
